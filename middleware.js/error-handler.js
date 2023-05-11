7f9728f5716f8a9f15ec0008c75172d3b57b51be
Abdullah <abdallah0115019@gmail.com>
2023-05-11 22:40:50 +0300

    error handler

--- middleware.js/error-handler.js
+++ middleware.js/error-handler.js
@@ -1,4 +1,8 @@
+const { customErrorHandler } = require("../errors/custom-error");
 const errorHandlerMidlleware = (err,req,res,next)=>{
+    if(err instanceof customErrorHandler){
+        res.status(err.statusCode).json({msg:err.message})
+    }
     res.status(500).json( {msg:"something wrong ,pleas try again"});
 }
 
