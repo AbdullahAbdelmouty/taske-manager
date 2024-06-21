df070a01d909300582262635f83dc6d76a144df9
Abdullah <abdallah0115019@gmail.com>
2024-06-21 11:39:35 +0300

    update

--- public/script.js
+++ public/script.js
@@ -167,7 +167,7 @@ const showAllTasks = (res)=>{
             const localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000 ); 
             e.target.children[0].children[2].value = localDate.toISOString().substring(0, 16);
             axios.post(`${API}`,formDataObj)
-            .then(res=>console.log(res))
+            .then(res=>console.log("done"))
             .catch(error=>console.log(error))
             // //add new task
             // axios.get(`${API}`).then(res=>{
